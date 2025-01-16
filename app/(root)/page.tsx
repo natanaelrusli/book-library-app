b740c0d7a5d5523b7d2166c97ea67aa10bfd59ce
Nata Nael <natanael280198@gmail.com>
2025-01-16 10:59:11 +0700

    feat: setup and base layout components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+
+const Home = () => (
+  <>
+    <BookOverview />
+    <BookList />
+  </>
+);
+
+export default Home;
---
+++ components/BookList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BookList = () => (
+  <section>
+    <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
+  </section>
+);
+
+export default BookList;
